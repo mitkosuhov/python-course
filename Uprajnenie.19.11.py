fd2d5b9236e12d1dc281fffb3fc3e970ef8eaa1d
mitkosuhov <mitkosuhov@gmail.com>
2023-11-19 21:59:51 +0200

    NightJob

--- Uprajnenie.19.11.py
+++ Uprajnenie.19.11.py
@@ -11,3 +11,4 @@ list1 = [7, 14, 21, 28, 35, 42]
 list2 ={2:'Pink', 4:'Red',5:'Blue',6:'Brown',7:'White'}
 print(list1[5])
 print(list2[7])
+print(1,2)
---
+++ homework/HomeworkClass2.19.11.py
@@ -0,0 +1,10 @@
+num1 = 20
+num2 = 7.5
+print (num1 // num2)
+name = ' Mitko'
+midname = ' Plamenov'
+surname = ' Suhov'
+print( name + midname + surname)
+days = ['Monday','Tosday','Wendsday','Thursday','Fraiday']
+coolors = {1:'White',3:'Black',5:'Red',7:'Green',9:'Blue'}
+print ( days[4] ,coolors[9] )
