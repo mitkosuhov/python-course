c41bf5163a2a1ede62258604b27531fd5e18416b
mitkosuhov <mitkosuhov@gmail.com>
2023-11-19 23:11:18 +0200

    Last one

--- homework/HomeworkClass2.19.11.py
+++ homework/HomeworkClass2.19.11.py
@@ -4,7 +4,7 @@ print (num1 // num2)
 name = ' Mitko'
 midname = ' Plamenov'
 surname = ' Suhov'
-print( name + midname + surname)
+print( name +'\n'+ midname +'\n'+ surname)
 days = ['Monday','Tosday','Wendsday','Thursday','Fraiday']
 coolors = {1:'White',3:'Black',5:'Red',7:'Green',9:'Blue'}
 print ( days[4] ,coolors[9] )
