2a2b00043f099cacc670a861a5d1ac87e0920b57
mitkosuhov <mitkosuhov@gmail.com>
2023-11-19 16:00:08 +0200

    Change

--- test19.11.py
+++ test19.11.py
@@ -4,9 +4,8 @@ boi = ['Каро ','Купа','Спатия','Пика']
 num = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
 
 i = 0
-while i < 5:
+while i < 52:
     print(random.choice(boi)+random.choice(num))
     i += 1
 
-print('New')
 
