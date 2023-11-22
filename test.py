64c82b168062a3a63b09ca8a99e3a8ddeb7b90a5
mitkosuhov <mitkosuhov@gmail.com>
2023-11-22 20:14:42 +0200

    new

--- test.py
+++ test.py
@@ -1,3 +1,15 @@
-num1 = 12
-num2 = 14
-print('My numbers are ', num1 , num2)
+#word1 = 0
+#word2 = 12,5
+#word3 ='12'
+#word4 = True
+#word5 = bool(word3)
+#print ( word1 , word2 , word3 , word4)
+#print ( type(word4))
+#print ( type(word5))
+#print ( word5)
+
+num1 = 7 
+num2 = 3
+print ( num1 % num2 )
+print ( num1 // num2 )
+print ( num1 /num2 )
