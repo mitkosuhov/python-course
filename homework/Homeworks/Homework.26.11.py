3aafa496dcc3019d7c732f48633065b1f11dc7b2
mitkosuhov <mitkosuhov@gmail.com>
2023-11-28 21:14:24 +0200

    new

--- homework/Homeworks/Homework.26.11.py
+++ homework/Homeworks/Homework.26.11.py
@@ -1,9 +1,10 @@
 str1 = 'Homeworksandotherstuffihavetoputtinthistext'
 letters = [i for i in str1]
 num_of = int((len(letters))-1)
-num_mid = (num_of//2)+1
+num_mid = (num_of//2)
 j = letters[0],letters[num_mid],letters[num_of]
 print(''.join(j))
+
 
 
 
