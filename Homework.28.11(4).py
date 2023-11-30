4a9e969481039f433f6b918a818bfbbe0956df2d
mitkosuhov <mitkosuhov@gmail.com>
2023-11-30 23:41:44 +0200

    new

--- Homework.28.11(4).py
+++ Homework.28.11(4).py
@@ -1,3 +1,12 @@
+days = int(input('How much days do you want to enter ?'))
+days_add =[]
+while days > 0 :
+    days_enter = input(f'Enter {days} day of the week :')
 
+    if days_enter in ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']:
+        days_add.append(days_enter)
+        days = days - 1
+    else :
+        print('Invalid day ')
 
-
+print(days_add)
