06ea1575f1e7f026cbf42391b0f15af6c31b2a90
mitkosuhov <mitkosuhov@gmail.com>
2023-12-12 14:08:58 +0200

    1

--- TestDecember.py
+++ TestDecember.py
@@ -1,9 +1,10 @@
 import os
 from pathlib import Path
 import random
+import shutil
 
 path_to = Path.home() / 'Desktop' / 'HomeWork.12.12'
-
+folders = []
 for x in range(100):
     while True :
         ran = str(random.randint(1,200))
@@ -12,11 +13,17 @@ for x in range(100):
 
             with open(file_path,'w') as file :
                 break
-                
 
 for num in range(1,10):
     num = str(num)
     direc = path_to / num
+    folders.append(direc)
     if not direc.exists():
         direc.mkdir()
     
+
+whit_2 = [file for file in path_to.iterdir() if file.is_file() and file.name.startswith('2')] 
+
+for file in whit_2:
+    
+    shutil.move(file, folders[1] / file.name)
