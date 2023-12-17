2fe5c7d05d0179bce48a2e2ea7e7977dbb8ddfe2
mitkosuhov <mitkosuhov@gmail.com>
2023-12-17 16:43:14 +0200

    l

--- ClassWork.17.12.py
+++ ClassWork.17.12.py
@@ -30,7 +30,41 @@
 #         for i in range(2,int(num**0,5)+1) :
 #             print (i)
 
-num = int(input('Enter a height :'))
-brick = 1
-for x in range(num+1):
-    print()
+# num = int(input('Enter a height :'))
+# brick = 1
+# for x in range(num+1):
+#     print()
+# name = 'Jesaa29Roy'
+# size = len(name)
+# i = 0
+# while i < size:
+#     if name[i].isdecimal():
+#         break
+#     print(name[i],end=' ')
+#     i += 1
+
+# size = int(input('How many blocks :'))
+
+# for i in range (1 , size+1):
+#     for j in range(1 ,i + 1):
+#         print(j , end=' ')
+#     print()        
+
+# num = ['2','3','4','5','6','7','8','9','10','j','q','k','a']
+# num2 = ['spi','cris','hart','pik']
+# final = []
+
+# for i in num :
+#     for j in num2:
+        
+#         final.append(j+i)
+# print(final)
+# print(len(final))
+
+
+
+        
+
+   
+
+    
