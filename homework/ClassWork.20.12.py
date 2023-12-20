2a3bbcbd0805b6cb2323d2949db522992840e8b5
mitkosuhov <mitkosuhov@gmail.com>
2023-12-20 21:26:57 +0200

    new

---
+++ homework/ClassWork.20.12.py
@@ -0,0 +1,140 @@
+#Задача 1
+# for i in range(1,6):
+#     print(i)
+
+#Задача 2 
+
+# import random
+
+# for i in range(1,200):
+#     print(random.randint(1,1000))
+
+# 3
+# for i in range(1,11,2):
+#     print(i+1)
+
+# 4 
+
+# for i in range(0,1):
+#     for j in range(0,5):
+#         if j == 0 or j == 4:
+#             print('****')
+#         elif j==1 or j== 2:
+#             print("*  *")    
+
+
+# for i in range(0,5):
+#     for j in range(1)
+
+    
+         
+# 5 
+# days = ('Monday','Thusday','Wednesday','Thursday','Friday','Saturday','Sunday')                
+    
+# for i in days :
+#     print(i)    
+            
+# 6 
+
+# words = 'Веселaaaaa'
+# count = 0 
+# while count < len(words):
+#     print(words[count])
+#     count +=1
+    
+# words = 'Веселaaaaa'
+# count = 0 
+# while count < len(words):
+#     print(words[count])
+#     count -=1
+        
+# 7 
+# for i in range(1,11):
+#     print(i)
+
+#8
+    
+# nums = [2,3,5,7,8,9,11]
+# nums_on = []
+# for i in nums:
+#     new_num = i**2    
+#     nums_on.append(new_num)
+# print(nums_on)    
+
+#9
+# num = int(input('Enter a number :'))
+
+# while num <= 100:
+#     num=num**2
+#     print(num)
+    
+#10
+# count = 1
+# while count <= 10 :
+#     print(count)
+#     count += 1    
+#     if count == 5 :
+#         break
+# #11
+# count = 0
+# while count <= 10 :
+#     count += 1
+#     if count == 3 :
+#         continue
+
+#     print(count)
+    
+#12
+    
+# nums = [1,2,3,4,5,6,7,8,9,10,11]
+# mod_nums = []
+# for i in nums:
+#     if i % 2 == 1 :
+#         nums.remove(i)
+# print(nums)
+        
+#13 
+
+# students = {'Mitko':5,"Ivan":4,'Petar':6}
+# for i in students :
+#     print(i)
+#     print(students[i])
+
+#14
+
+# collors = ['red','green','blue','white']
+
+# for i,y in enumerate(collors):
+#     print(i)
+#     print(y)
+    
+    
+#15 Fibonachi numbers
+nums= [1,1]
+count = 1
+b=nums[0]
+c=nums[1]
+
+while count < 10 :
+    a = b +c 
+    print(a)
+    c = b
+    b = a
+    
+    count +=1
+
+
+
+
+    
+
+
+    
+    
+
+
+
+   
+    
+        
+    
