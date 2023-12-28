b4531e54ce7e3e6fa28de507dcba167cd74bc9cd
mitkosuhov <mitkosuhov@gmail.com>
2023-12-28 23:10:14 +0200

    1

--- homework/Homeworks/CrismassHomework2023.py
+++ homework/Homeworks/CrismassHomework2023.py
@@ -9,15 +9,15 @@ while True :
                 q2 = input('Описание на задача?')
                 task = [q1,q2]
                 tasks.append(task)
-                q3 =input('Искате ли да въведете още задачи ,ако не (stop):')
-                if q3 == 'stop':                   
+                q3 =input('Искате ли да въведете още задачи ,ако не (стоп):')
+                if q3 == 'стоп':                   
                     break
                 elif q3 == 'искам' or q3 == 'да':
                     continue    
                 else :
                     print('Грешна команда')
                     break
-            elif q1 == 'stop':
+            elif q1 == 'стоп':
                 break  
     elif action1 == '2' : 
         while True:
@@ -28,7 +28,7 @@ while True :
                 for i in tasks :
                     if q4 in i :
                         print(i)
-                q5 =input('Искате ли да прегледате още задачи ,ако не (stop):')
+                q5 =input('Искате ли да прегледате още задачи ,ако не (стоп):')
                 if q5 == 'stop':                   
                         break
                 elif q5 == 'искам' or q5 == 'да':
@@ -36,40 +36,45 @@ while True :
                 else :
                         print('Грешна команда')
                         break    
-            elif q4 == 'stop':
+            elif q4 == 'стоп':
                 break            
     elif action1 == '3':
          while True :
-              for i in enumerate(tasks) :
+              for i in enumerate(tasks,start=1) :
                    print(i)                     
-              q7 =int(input('Коя задача искате да промените :')) 
-              if q7 <= (len(tasks)-1):
-                   print(tasks[q7])
-                   q8 = input('Избери 0 за промяна на заглавието , 1 за промяна на описанието 2 за промяна на позицията на задачите:')
-                   if q8 == '0' :
-                        q9 = input('Изберете ново заглавие :')
-                        tasks[q7][0] = q9
-                        print(tasks[q7])
-                   elif q8 == '1':
-                        q10 = input('Изберете ново описание :')
-                        tasks[q7][1] = q10
-                        print(tasks[q7])
-                   elif q8 == '2':
-                        q10 = int(input(f'С коя задача искате да смените позията на {tasks[q7]} '))
-                        if q10 <= (len(tasks)-1):
-                            
-                            tasks[q7],tasks[q10] = tasks[q10], tasks[q7]
-                        else :
-                             print('Грешен избор !!!')    
-                      
+              q7 =input('Коя задача искате да промените , за изход напишете (стоп):')
+              if q7 == 'стоп':
+                   break
+              elif q7.isdigit():
+                   q7=int(q7) 
+                   q7 -= 1
+                   if q7 <= (len(tasks)-1):
+                         print(tasks[q7])
+                         q8 = input('Избери 0 за промяна на заглавието , 1 за промяна на описанието 2 за промяна на позицията на задачите:')
+                         if q8 == '0' :
+                              q9 = input('Изберете ново заглавие :')
+                              tasks[q7][0] = q9
+                              print(tasks[q7])
+                         elif q8 == '1':
+                              q10 = input('Изберете ново описание :')
+                              tasks[q7][1] = q10
+                              print(tasks[q7])
+                         elif q8 == '2':
+                              q10 = int(input(f'С коя задача искате да смените позията на {tasks[q7]} '))
+                              q10 -= 1
+                              if q10 <= (len(tasks)-1):
+                              
+                                   tasks[q7],tasks[q10] = tasks[q10], tasks[q7]
+                              else :
+                                   print('Грешен избор !!!')    
+                                  
                    else :
-                        print('Грешен избор !!!')  
-              else :
-                   print('Греен избор !!!')
+                              print('Грешн избор !!!')
     elif action1 == '4':
-         for i in enumerate(tasks) :
+         for i in enumerate(tasks,start=1) :
               print(i)   
-         q11 = int(input('Коя задача искате да изтриете :'))      
+         q11 = int(input('Коя задача искате да изтриете :'))    
+         q11 -= 1  
          if q11 <= ((len(tasks))-1) :
               tasks.pop(q11)       
          else :
