56b4ea4d1106b74272dbe1830e632b6d82511461
mitkosuhov <mitkosuhov@gmail.com>
2024-01-05 23:37:35 +0200

    1

---
+++ homework/Homeworks/HomeWork.3.01.py
@@ -0,0 +1,14 @@
+# # Задача 1 :
+# def small(x,y,z,):
+#     return min(x,y,z,)
+
+# num1 = float(input('Enter number 1 :'))
+# num2 = float(input('Enter number 2 :'))
+# num3 = float(input('Enter number 3 :'))
+
+# print(f'The smallest number is {small(num1,num2,num3)}')
+
+# Задача 2 :
+def sum_of(x,y,z):
+    
+    return ()
