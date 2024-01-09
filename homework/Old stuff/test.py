70e1e339f794a2249de4c268a202adeb789774ea
mitkosuhov <mitkosuhov@gmail.com>
2024-01-09 11:13:02 +0200

    1

--- homework/Old stuff/test.py
+++ homework/Old stuff/test.py
@@ -1,55 +1,24 @@
-# lista = (3,6,9)
-# i = list(lista)
-# i[1] = 7 # разопаковаме листа 
-# print(tuple(i))
+from tkinter import *
+
+tasks = [
+    ['Почистване', 'Почистване на общи части'],
+    ['Преместване', 'Преместване на стари елементи'],
+    ['Сортиране', 'Сортиране на нови елементи'] ]
+
+root = Tk()
+root.geometry('400x230')
+root.configure(bg='#a27341')
 
 
+button1 = Button(root, text='Въвеждане на задача', bg='#4d98d0')
+button2 = Button(root, text='Преглед на задача',bg='#4d98d0')
+button3 = Button(root, text='Редактирване на задача',bg='#4d98d0')
+button4 = Button(root, text='Изтриване на задача',bg='#4d98d0')
+button5 = Button(root, text='Изход',command=root.quit,bg='#da3636')
 
-# name = {1:'Blue',2:'Red',3: {1:'name',2:'age',3:{1:'Place',2:'Gas',3:'Tree'}}}
-# print(name[3][3][2])
-
-
-# song = "Let it be , let it be , let it be , let it be "
-
-# print(song.replace('let it be ','dont let it be ',2))
-
-# nums = [1,3,5,6,8,10.12]
-# filter_nums = [i for i in nums if i > 4 and i % 2 == 0 ] # Филтър 
-# print(filter_nums)
-
-
-
-# #Подобрена домашна 
-# str1 = 'Homeworksandotherstuffihavetoputtinthistext'
-# len_str1= int((len(str1))/2)
-# print(str1[0::len_str1])
-
-# # ФизБъз задача
-# for fizzbuzz in range(51):   
-#     if fizzbuzz % 3 ==0 and fizzbuzz % 5 == 0 :
-#         print('FIzzbuzz')
-#     elif fizzbuzz % 3 ==0 :
-#         print('Fizz')
-#     elif fizzbuzz % 5== 0 :
-#         print('Buzz')   
-#     else :
-#         print(fizzbuzz)    
-
-
-#  # Drug primer 
-#  # list_nums = []
-# for nums in range(1500,2701):
-#     if nums % 5 == 0 and nums % 7 == 0 :
-#         nums=str(nums)
-#         list_nums.append(nums)
-
-# print(list_nums)
-# print('/'.join(list_nums))
-       
-tasks = [['Почистване', 'Почистване на общи части'], ['Преместване', 'Преместване на стари елементи'], ['Сортиране', 'Сортиране на нови елементи']]
-
-for i in enumerate(tasks) :
-    print(i[0])
-ask = int(input('Коя задача искате да промените '))
-if ask <= len(tasks):
-    print(tasks[ask])
+button1.pack(padx=2, pady=2)
+button2.pack(padx=2, pady=2)
+button3.pack(padx=2, pady=2)
+button4.pack(padx=2, pady=2)
+button5.pack(side=BOTTOM,anchor=SE,padx=20, pady=20)
+root.mainloop()
