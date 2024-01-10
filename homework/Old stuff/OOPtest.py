abc92d36e5af367a4c641f51014a7e424339c7bc
mitkosuhov <mitkosuhov@gmail.com>
2024-01-10 21:25:35 +0200

    1

---
+++ homework/Old stuff/OOPtest.py
@@ -0,0 +1,8 @@
+import OOP
+import Subject
+
+studentOne = Student('Petar','Ivanov',90121)
+studentTwo = Student('John','Dow',1234)
+StudentThree = Student('Mitko','Ivan',1231)
+
+Onefitness = [Subject]
