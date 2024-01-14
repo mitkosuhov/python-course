eba4a3625b41e51ac838b0fdbc229df154ec1f2e
mitkosuhov <mitkosuhov@gmail.com>
2024-01-14 16:55:19 +0200

    1

---
+++ homework/Homeworks/HomeWork14.01.24.py
@@ -0,0 +1,38 @@
+# Задача 1
+# class Person():
+#     def __init__(self,name,age):
+#         self.__name = name 
+#         self.__age = age
+
+#     def get_name(self):
+#         print(self.__name)    
+#     def get_age(self):
+#         print(self.__age)    
+
+# persone1 = Person('Иван Иванов',28)
+# persone1.get_name()
+# persone1.get_age()
+
+# Задача 2 
+# class Mammal():
+#     def __init__(self,name,type,sound):
+#         self.name = name
+#         self.type = type
+#         self.sound = sound
+#         self.kingdom = 'Animals'
+
+#     def make_sound(self):
+#         print(f'{self.name} make the sound {self.sound}')
+
+#     def kingdomm(self):
+#         print(self.kingdom)    
+
+#     def info(self):
+#         print(f'{self.name} is {self.type} type of animals ')   
+
+# cat = Mammal('Cat','Home ','Meow')
+# cat.make_sound()
+# cat.kingdomm()
+# cat.info()
+
+# Задача 3 
