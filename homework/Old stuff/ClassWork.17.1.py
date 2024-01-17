0e5492d89181da9fd888bf3ed95ae3ac45273dc2
mitkosuhov <mitkosuhov@gmail.com>
2024-01-17 21:23:57 +0200

    1

---
+++ homework/Old stuff/ClassWork.17.1.py
@@ -0,0 +1,13 @@
+class BookColection :
+    name = 24
+    def __init__(self):
+        self.book = 10
+        self.names = 21
+        self.age = 5
+
+
+b1=BookColection()
+print(b1.name)
+
+    
+        
