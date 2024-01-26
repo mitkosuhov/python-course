b7ff3bb9519f17d55c3654fa4e2e0c400fe33544
mitkosuhov <mitkosuhov@gmail.com>
2024-01-26 22:35:54 +0200

    1

---
+++ homework/Homeworks/HomeWork.24.01.py
@@ -0,0 +1,62 @@
+#Задача 1
+# import requests
+
+# s = requests.get("https://pypi.org/project/requests/")
+
+# print(s.text)
+
+# Задача 2
+# from math import sqrt
+
+# num = sqrt(49)
+# print(num)
+
+#Задача 3 
+
+# class Calculator :
+
+#     def add(self,x,y):
+#         return x+y
+    
+#     def subtract(self,x,y):
+#         return x-y
+    
+#     def multi(self,x,y):
+#         return x*y
+    
+#     def divide(self,x,y):
+#         if y != 0 :
+#             return x/y 
+#         else:
+#             return 'Не се дели на нула '
+
+# result = Calculator()
+
+# print(result.add(3,6))
+# print(result.subtract(9,7))
+# print(result.multi(4,4))
+# print(result.divide(94,4))
+
+
+
+#Задача 4
+
+# import datetime as dt
+
+# print(dt.datetime.now())
+#Задача 5
+# import os 
+
+# print(os.getcwd())
+# os.chdir(r'C:\Users\Mitko\Desktop\HomeWork.12.12')
+# print(os.getcwd())
+# os.chdir(r"C:\Users\Mitko\Desktop\python2023")
+# print(os.getcwd())
+
+#Задача 6
+
+# import random
+
+# for i in range(5):
+#     print(random.choice(range(1,10)))
+
