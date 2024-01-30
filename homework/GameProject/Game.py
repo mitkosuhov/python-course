9fd86196046f549551bb87de4b2519a1494e5441
mitkosuhov <mitkosuhov@gmail.com>
2024-01-30 10:54:06 +0200

    1

--- homework/GameProject/Game.py
+++ homework/GameProject/Game.py
@@ -5,10 +5,10 @@ screen = pygame.display.set_mode((800,400))
 clock = pygame.time.Clock()
 font= pygame.font.Font(r'C:\Users\Mitko\Desktop\python2023\homework\GameProject\Pixeltype.ttf',70)
 
-surface = pygame.image.load(r'C:\Users\Mitko\Desktop\python2023\homework\GameProject\blue.jpeg')
-grass = pygame.image.load(r'C:\Users\Mitko\Desktop\python2023\homework\GameProject\grd.png')
-test_font = font.render('Igrata mi ',False,'#3d7724')
-cat = pygame.image.load(r'C:\Users\Mitko\Desktop\python2023\homework\GameProject\cat.jpg')
+surface = pygame.image.load(r'C:\Users\Mitko\Desktop\python2023\homework\GameProject\blue.jpeg').convert()
+grass = pygame.image.load(r'C:\Users\Mitko\Desktop\python2023\homework\GameProject\gr.jpg').convert_alpha()
+test_font = font.render('Igrata mi ',False,'#3d7724').convert()
+cat = pygame.image.load(r'C:\Users\Mitko\Desktop\python2023\homework\GameProject\cat.jpg').convert_alpha()
 cat_posicion = 600
 
 while True:
@@ -18,12 +18,12 @@ while True:
             exit()
     
     screen.blit(surface,(0,0))
-    screen.blit(grass,(0,100))
-    screen.blit(grass,(400,100))
+    screen.blit(grass,(0,350))
+
     screen.blit(test_font,(300,50))
     cat_posicion -= 5
     if cat_posicion < -100: cat_posicion = 800
-    screen.blit(cat,(cat_posicion,150))
+    screen.blit(cat,(cat_posicion,250))
 
     pygame.display.update()        
     clock.tick(60)
