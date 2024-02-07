7b4300c5a148d21ee27b2cdbe56e540664bbf9c2
mitkosuhov <mitkosuhov@gmail.com>
2024-02-07 21:16:43 +0200

    1

---
+++ homework/ClassE/Class.7.2.py
@@ -0,0 +1,36 @@
+# def algor(x):
+#     first = x[0]
+#     sort = [first]
+#     for i in x [1:] :
+#         if i > first :
+#             sort.append(i)
+#         else :
+#             sort.insert(0 , i)     
+#     return sort        
+# nums = [101,3,5,7,12,4,2,3,65,78,99,1,2,31,74,8,35]
+
+
+# print(algor(nums))
+
+# def fact(x):
+#     if x ==1:
+#         return 1
+#     else:
+#         return x* fact(x-1)
+
+# print(fact(5))
+
+
+# def fib(x):
+#     if x <= 1 :
+#         return x 
+#     else:
+#         return fib(x-1) + fib(x-2)
+
+# print(fib(35))
+
+
+def dfs(x,y,z):
+    if y not in z :
+        x
+    
