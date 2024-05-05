3f128e0d4c37cc4022116b5de747646c9188d9e6
mitkosuhov <mitkosuhov@gmail.com>
2024-05-05 21:42:34 +0300

    1

--- homework/ClassE/test.py
+++ homework/ClassE/test.py
@@ -1,18 +1,11 @@
-nums = [2,4,6,4,3,1,5,7,9,6,4,2,1,1,8,0,5,4,3]
-num1 = 3
+nums = 7000000000
 
 
-def del_chek(x,y):
-    ansers = []
-    for i in x :
-        if i%y == 0  :
-            ansers.append(i) 
-    return ansers        
-        
-               
+def coltdown(x):
+    counter = 0 
+    while x > 1 :
+        x = x//2
+        print(f'Run is {counter} : {x}')
+        counter += 1
 
-
-
-print(del_chek(nums,num1))
-
-
+print(coltdown(nums))
