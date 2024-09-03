517382b2dd0dda991a4c97482738450878491cb1
mitkosuhov <mitkosuhov@gmail.com>
2024-09-03 23:55:40 +0300

    1

--- homework/ClassE/test.py
+++ homework/ClassE/test.py
@@ -1,11 +1,14 @@
-import numpy as np
+Sep = 30 
+Oct = 31
+Nov = 30 
+Dec = 31
+work = 0
 
-# Създаване на матрици с NumPy
-matrix1 = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-matrix2 = np.array([[2, 2, 2], [3, 3, 3], [4, 4, 4]])
 
-# Извършване на матрично умножение
-result = np.dot(matrix1, matrix2)
-
-# Отпечатване на резултата
-print(result)
+for i in range(Sep):
+    if work <= 1:
+        print(f'{i+1} работен')  
+        work +=1 
+    elif work > 1:
+        print(f'{i+1} почивен')
+        work -= 1       
