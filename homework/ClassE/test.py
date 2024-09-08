8be5f06b34bcde25fbe307031faca445dbdd09ff
mitkosuhov <mitkosuhov@gmail.com>
2024-09-08 19:49:31 +0300

    1

--- homework/ClassE/test.py
+++ homework/ClassE/test.py
@@ -1,14 +1,6 @@
-Sep = 30 
-Oct = 31
-Nov = 30 
-Dec = 31
-work = 0
+n = int(input('Enter n :'))
 
-
-for i in range(Sep):
-    if work <= 1:
-        print(f'{i+1} работен')  
-        work +=1 
-    elif work > 1:
-        print(f'{i+1} почивен')
-        work -= 1       
+for i in range(n):
+    for j in range(n):
+        print('*',end='')
+    print()    
