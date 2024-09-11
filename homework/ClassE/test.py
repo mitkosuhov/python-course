f20bf4fac37a13c972fa36dfb2789fea4fb02c42
mitkosuhov <mitkosuhov@gmail.com>
2024-09-11 20:43:50 +0300

    1

--- homework/ClassE/test.py
+++ homework/ClassE/test.py
@@ -1,6 +1,10 @@
-n = int(input('Enter n :'))
+n = 5
 
 for i in range(n):
-    for j in range(n):
+    for j in range(i+1):
+        print(' ',end='')
+    for h in range(n-i):
+        print('*',end='')  
+    for z in range(n-i):
         print('*',end='')
-    print()    
+    print()
