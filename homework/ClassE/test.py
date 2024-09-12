a95891cb894ab5bd503a45d41f2dbf6ff8b31fb9
mitkosuhov <mitkosuhov@gmail.com>
2024-09-13 00:22:26 +0300

    1

--- homework/ClassE/test.py
+++ homework/ClassE/test.py
@@ -1,10 +1,11 @@
-n = 5
+matrix = [[1, 2, 3],
+          [4, 5, 6],
+          [7, 8, 9]]
+print("Оригинална матрица:")
+for row in matrix:
+    print(row)
+transposed_matrix = [[row[i] for row in matrix] for i in range(len(matrix[0]))]
+print("\nТранспонирана матрица:")
 
-for i in range(n):
-    for j in range(i+1):
-        print(' ',end='')
-    for h in range(n-i):
-        print('*',end='')  
-    for z in range(n-i):
-        print('*',end='')
-    print()
+for i in transposed_matrix:
+    print(i)
